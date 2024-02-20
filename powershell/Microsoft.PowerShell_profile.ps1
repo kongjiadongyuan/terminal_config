08cd616589fd2e70ac25ab734b52e51a722dee2a
张伯伦 <zhangbolun181@mails.ucas.ac.cn>
2024-02-20 09:59:02 +0800

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,21 @@
 function ssh {
-    param([string] $target_host)
-    $ssh_config_path = "$home\.ssh\ssh_config.json"
     $builtin_ssh = (Get-Command ssh -CommandType Application -All)[0].Source
-    # Write-Host $builtin_ssh
-    if (Test-Path $ssh_config_path) {
-        # Write-Host  "Using ssh_config.json"
-        $ssh_config = Get-Content -Raw -Path $ssh_config_path | ConvertFrom-Json
-        if ($ssh_config.PSObject.Properties.Name -contains $target_host) {
-            # Write-Host "Connecting to $target_host"
-            $command_to_execute = $ssh_config.$target_host
-            # Write-Host "Executing $command_to_execute"
-            $exp = "& `"$builtin_ssh`" -t $target_host `"$command_to_execute`""
-            # Write-Host $exp
-            Invoke-Expression $exp
-            return
+    if ($args.Count -eq 1) {
+        $ssh_config_path = "$home\.ssh\ssh_config.json"
+        if (Test-Path $ssh_config_path) {
+            $target_host = $args[0]
+            $ssh_config = Get-Content -Raw -Path $ssh_config_path | ConvertFrom-Json
+            if ($ssh_config.PSObject.Properties.Name -contains $target_host) {
+                $command_to_execute = $ssh_config.$target_host
+                $exp = "& `"$builtin_ssh`" -t $target_host `"$command_to_execute`""
+                Invoke-Expression $exp
+                return
+            }
         }
     }
-    Invoke-Expression "& `"$builtin_ssh`" -t $target_host $args"
+
+    $exp = "& `"$builtin_ssh`" $args"
+    Invoke-Expression $exp
 }
 
 function sssh {
