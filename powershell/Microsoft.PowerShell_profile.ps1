d4c6ef524b877c8860d3961d331cb290eb6c8e0d
张伯伦 <zhangbolun181@mails.ucas.ac.cn>
2024-03-04 12:05:09 +0800

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,12 @@ function ssh {
             $ssh_config = Get-Content -Raw -Path $ssh_config_path | ConvertFrom-Json
             if ($ssh_config.PSObject.Properties.Name -contains $target_host) {
                 $command_to_execute = $ssh_config.$target_host
+
+                # For windows terminal tab name
+                $host.ui.RawUI.WindowTitle = $target_host
+
                 $exp = "& `"$builtin_ssh`" -t $target_host `"$command_to_execute`""
+                Write-Host "Executing: $exp"
                 Invoke-Expression $exp
                 return
             }
@@ -15,10 +20,12 @@ function ssh {
     }
 
     $exp = "& `"$builtin_ssh`" $args"
+    Write-Host "Executing: $exp"
     Invoke-Expression $exp
 }
 
 function sssh {
     $builtin_ssh = (Get-Command ssh -CommandType Application -All)[0].Source
+    Write-Host "Executing: $exp"
     Invoke-Expression "& `"$builtin_ssh`" $args"
 }
